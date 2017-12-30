593fb24daea189bde6ddd94ac5c398ad3837ef3c
Christopher Mühl <github@padarom.io>
2017-12-30 23:09:39 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+const logger = require('winston');
+const fs = require('fs');
+
+fs.readFile('daemon.pid', (err, data) => {
+  if (!err) {
+    logger.error('Tesseracat is already running with PID %s.', data);
+    process.exit(1);
+  }
+
+  logger.info('Daemonizing Tesseracat now.');
+
+  // Daemonize now.
+  require('daemon')();
+
+  logger.info('Successfully daemonized with PID %s.', process.pid);
+
+  fs.writeFileSync('daemon.pid', process.pid);
+  process.on('exit', killApplication);
+  process.on('SIGTERM', killApplication);
+
+  startApplication();
+});
+
+function startApplication() {
+  const app = require('./app');
+  const port = app.get('port');
+  const server = app.listen(port);
+  
+  process.on('unhandledRejection', (reason, p) =>
+    logger.error('Unhandled Rejection at: Promise ', p, reason)
+  );
+  
+  server.on('listening', () =>
+    logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+  );
+}
+
+function killApplication() {
+  fs.unlinkSync('daemon.pid');
+}
