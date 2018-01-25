a8ba393f1517bdc308864a6da133b1e90522630b
Christopher Mühl <christopher@dieschittigs.de>
2018-01-25 19:11:00 +0100

    Silence the db-migrate logger

--- src/iot/Migrator.js
+++ src/iot/Migrator.js
@@ -1,13 +1,19 @@
 let dbmigrate = require('db-migrate')
+let log = require('db-migrate-shared').log
+let logger = require('../Logger')
 
 module.exports = class Migrator {
   constructor () {
     this.dbm = dbmigrate.getInstance(true, {
       config: 'config/database.json'
     })
+
+    // Silence the default db-migrate log
+    log.silence(true)
   }
 
   migrate () {
+    logger.info('Making sure the database is migrated.')
     return this.dbm.up()
   }
 }
