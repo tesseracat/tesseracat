13270d131a6647ffcd70a8a36403beccd4c0a4b9
Christopher Mühl <christopher@dieschittigs.de>
2018-02-20 15:42:40 +0100

    Serve static files

--- src/koa/index.js
+++ src/koa/index.js
@@ -1,24 +1,35 @@
-const Koa = require('koa')
-const KoaRouter = require('koa-router')
-const koaBody = require('koa-bodyparser')
-const { graphqlKoa, graphiqlKoa } = require('apollo-server-koa')
-const { makeExecutableSchema } = require('graphql-tools')
-const context = require('../Storage')
+const path = require('path')
 
-const app = new Koa();
+const Koa = require('koa')
+const koaBody = require('koa-bodyparser')
+const KoaRouter = require('koa-router')
+const serve = require('koa-static')
+
+const { makeExecutableSchema } = require('graphql-tools')
+const { graphqlKoa, graphiqlKoa } = require('apollo-server-koa')
+
 const router = new KoaRouter();
 const port = 3030;
+const app = new Koa();
 
+// Generate a context and GraphQL schema
+const context = require('../Storage')
 let schema = makeExecutableSchema({ typeDefs: require('./schema'), resolvers: require('./resolver') })
 
+// Serve GraphQL queries, migrations and graphiql debugger
 router.post('/graphql', koaBody(), graphqlKoa({ schema, context }))
 router.get('/graphql', graphqlKoa({ schema, context }))
-router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }))
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }))
+}
 
-router.get('/', (ctx, next) => {
-  ctx.body = 'h'
-})
+// Serve static files from @iotame/web
+app.use(serve(path.resolve(__dirname, '../../node_modules/@iotame/web/dist'), {
+  defer: true,
+  gzip: true
+}))
 
+// Register the router
 app.use(router.routes())
 app.use(router.allowedMethods())
 
