6d0076f8153a36b8f5cb3509869e3b23f7b0221b
Christopher Mühl <christopher@dieschittigs.de>
2018-02-21 10:28:49 +0100

    Implement sliding expiration for JWT

--- src/koa/index.js
+++ src/koa/index.js
@@ -4,6 +4,8 @@ const Koa = require('koa')
 const koaBody = require('koa-bodyparser')
 const KoaRouter = require('koa-router')
 const serve = require('koa-static')
+const koaJwt = require('koa-jwt')
+const jwt = require('jsonwebtoken')
 
 const { makeExecutableSchema } = require('graphql-tools')
 const { graphqlKoa, graphiqlKoa } = require('apollo-server-koa')
@@ -11,6 +13,8 @@ const { graphqlKoa, graphiqlKoa } = require('apollo-server-koa')
 const router = new KoaRouter();
 const port = 3030;
 const app = new Koa();
+
+let secret = 'generated-secret'
 
 // Generate a context and GraphQL schema
 const context = require('../Storage')
@@ -22,6 +26,27 @@ router.get('/graphql', graphqlKoa({ schema, context }))
 if (process.env.NODE_ENV !== 'production') {
   router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }))
 }
+
+// Add new JWT token to response if close to expiry
+app.use(async (ctx, next) => {
+  ctx.state.secret = secret
+  await next()
+
+  // Only continue if token is set
+  if (ctx.state.user) {
+    let {iat, exp, ...claims } = ctx.state.user
+    let lifespan = exp - iat
+    let now = Math.floor(Date.now() / 1000)
+
+    // Re-issue a token, if half its lifetime is over
+    if (now > ctx.state.user.iat + (lifespan / 2)) {
+      ctx.set('Set-Authorization', jwt.sign(claims, secret, { expiresIn: lifespan }))
+    }
+  }
+})
+
+// Verify JWT tokens on requests
+app.use(koaJwt({ passthrough: true }))
 
 // Serve static files from @iotame/web
 app.use(serve(path.resolve(__dirname, '../../node_modules/@iotame/web/dist'), {
